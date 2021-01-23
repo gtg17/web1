ee4451bcb2ebcd97a7463634605417f6d5d304cd
gtg17 <75616916+gtg17@users.noreply.github.com>
2021-01-23 14:14:38 +0900

    Add files via upload

---
+++ 1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>WEB1 - Html </title>
+<meta charset = "utf-8">
+</head>
+<body>
+<h1><a href="1.php">WEB</a></h1>
+<ol>
+    <li><a href="2.php">목적</a></li>
+    <li><a href="3.php">저자</a></li>
+    <li><a href="">이유</a></li>
+</ol>
+<h2>개요</h2>
+앞으로는 개발자도 중요하고, 기본적인 역량이며 무엇보다 중요한것은 내가 <strong>배우고싶어서이다.</strong>
+
+</body>
+</html>
---
+++ 2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>WEB1 - Html </title>
+<meta charset = "utf-8">
+</head>
+<body>
+<h1><a href="1.php">WEB</a></h1>
+<ol>
+    <li><a href="2.php">목적</a></li>
+    <li><a href="3.php">저자</a></li>
+    <li><a href="4.php">이유</a></li>
+</ol>
+<h2>개요</h2>
+일단, 웹사이트 첫번째 100몇강의 다 들었구요, 이후 계속해서 들을겁니다. <strong>나만의 <u>홈페이지</u>를
+만들기 위해.</strong>
+
+</body>
+</html>
